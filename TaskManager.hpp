f623ad2bc972243b0ede14fada812606efd92d68
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2026-02-24 14:11:02 -0700

    Set-up header file

--- TaskManager.hpp
+++ TaskManager.hpp
@@ -1,7 +1,51 @@
 #ifndef TASK_MANAGER_HPP
 #define TASK_MANAGER_HPP
 
+#include <string>
 
+using namespace std;
 
+// ENUMS
+
+enum Status {
+    Todo,
+    InProgress,
+    Finished
+};
+
+enum Priority {
+    Low,
+    Medium,
+    High
+};
+
+enum Role {
+    Manager,
+    SeniorEmployee,
+    JuniorEmployee
+};
+
+// STRUCTS
+
+struct Task {
+    int id;
+    string title;
+    string description;
+    Status status = Todo;
+    Priority priorityLevel;
+    TeamMember assignee;
+};
+
+struct TeamMember {
+    int id;
+    string name;
+    Role role = JuniorEmployee;
+};
+
+// CLASSES
+
+class TaskManager {
+
+};
 
 #endif
